1641c27acb203d40d2017878f5fe9243d3f40bf3
Aarón Martin <aamarber@gmail.com>
2020-11-08 09:08:35 +0100

    Adding activities and their styling

---
+++ js/console.js
@@ -0,0 +1,13 @@
+const fontColor = '#bada55';
+            const titleFontSize = '500%';
+            const textFontSize = '200%';
+
+(function(){
+    console.log('%c¡Hola!', 'color: ' + fontColor + '; font-size: ' + titleFontSize + ';');
+    console.log('%cParece que has llegado hasta la consola de desarrolladores para desentrañar mis secretos... Pero no será tan sencillo...', 'color: ' + fontColor + '; font-size: ' + textFontSize + ';');
+    console.log('%cAl menos, devuélveme el saludo.', 'color: ' + fontColor + '; font-size: ' + textFontSize + ';');
+})();
+
+function hola(){
+    console.log('%cEso está mejor, un poco de educación. Para continuar con la caza ve a ', 'color: ' + fontColor + '; font-size: ' + textFontSize + ';');
+}
